3230f90e89f19c31956ee0465f894831d3265d20
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2024-05-12 20:40:19 -0400

    Add files via upload

---
+++ compValue.js
@@ -0,0 +1,18 @@
+//take a number, and say whether it's even or odd
+
+
+function evenOrOdd(number) {
+    return number % 2 === 0 ? 'Even' : 'Odd';
+  }
+
+
+
+
+
+  function evenOrOdd(number) {
+    if (number % 2 == 0){
+      return "Even"
+    }else{
+      return "Odd"
+      }
+  }
---
+++ numToString.js
@@ -0,0 +1,7 @@
+//take in a number, turn to a string
+
+function numberToString(num) {
+    return num.toString()
+   }
+
+   //could also use    return String(num);
