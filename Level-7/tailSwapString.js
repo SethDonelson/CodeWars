cd361aa180ccad17374f5097741ec4e6046ea19f
Seth Donelson <seth.donelson@gmail.com>
2024-07-13 19:19:42 -0400

    added 3 level 7 codewars

--- Level-7/tailSwapString.js
+++ Level-7/tailSwapString.js
@@ -16,6 +16,6 @@
 
   function tailSwap(arr) {
     let newArr = arr
-    .map(string => string.split(':'))
+    arr.map(string => string.split(':'))
     return [newArr[0][0]+':'+newArr[1][1], newArr[1][0]+':'+newArr[0][1]];
   }
