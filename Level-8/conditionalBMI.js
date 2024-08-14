29a19788478a0057213a6afff5e6ff8c63dfbf3e
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2024-08-13 20:04:47 -0400

    conditional using &&

--- Level-8/conditionalBMI.js
+++ Level-8/conditionalBMI.js
@@ -16,3 +16,15 @@ function bmi(weight, height) {
       return 'Obese'
     } 
   }
+
+//The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+  function setAlarm(employed, vacation){
+    if (employed == true && vacation == false){
+      return true
+    } else{
+      return false
+    }
+    
+  }
+  
