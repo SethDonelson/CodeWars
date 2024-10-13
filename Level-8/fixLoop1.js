33d3e3f255e5b857215071ec78b9b4449f294df4
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2024-10-12 20:36:59 -0400

    return all values from a to b in an arr

--- Level-8/fixLoop1.js
+++ Level-8/fixLoop1.js
@@ -9,3 +9,20 @@ function createArray(number){
   }
 
   //loop was missing counter++ so I'm thinking it'd just repeat and never add up to the number entered
+
+
+  //return values from a to b in an array
+
+  function between(a, b) {
+    let array = []
+    for(let i= a; i <=b; i++){
+      array.push(i)
+    }
+    return array
+  }
+ 
+ //between a and b, positive, numbers
+ //return array of integers betweeen a - b
+ //   1,4  = [1, 2, 3, 4]
+ //count from a to b with a loop, return all numbers in count
+ //for ( let i = a; i <=b; i++){
