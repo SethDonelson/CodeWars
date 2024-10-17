cb1c5587c66e6b2b43c2155e8e0df6211e57e2b6
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2024-10-17 13:11:55 -0400

    if else with num identification

--- Level-8/numToString.js
+++ Level-8/numToString.js
@@ -2,3 +2,21 @@
 function numberToString(num) {
     return num.toString()
    }
+
+
+   
+   //return *50 +6 if a number, else return error
+
+   function problem(x){
+    if(typeof x !== 'number'){ 
+      return "Error"} 
+    else { 
+      return (x * 50 + 6)
+    }
+  }
+  
+  //given a number, positive, integer or num
+  //return x50 + 6, or erro
+  // 1  = 56
+  // if(x != number){ return "Error"} else{ return ((x *50) +6)}
+  
