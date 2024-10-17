901381c138f764c3cf3f7cde9451c98cb7142dd4
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2024-10-17 13:51:10 -0400

    find  and add total vowels

---
+++ Level-7/countVowels.js
@@ -0,0 +1,11 @@
+//given a string, return number of vowels, excludes y
+
+function getCount(str){
+    let count = 0;
+    for (let i = 0; i <str.length; i++){
+      if( str.charAt(i) == "a" || str.charAt(i) == "e" || str.charAt(i) == "i" || str.charAt(i) == "o" || str.charAt(i) == "u"){
+        count += 1;
+      }
+    }
+    return count
+  }
