73fed9451effbb0b03844614d79c0669d99bb088
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2024-10-22 20:41:36 -0400

    check if neg and return neg num

--- Level-8/addThree.js
+++ Level-8/addThree.js
@@ -3,3 +3,20 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
   }
+
+
+
+  
+  function makeNegative(num) {
+    if(num <=0){
+      return num
+    }else {
+     return (-num)
+     }
+    
+  }
+  
+  //given any number, pos or neg, no strings,
+  //return neg even if neg, 0 is 0
+  //1 =-1, 0 = 0, -5 = -5
+  //check if negative, return num if yes, if not then convert
